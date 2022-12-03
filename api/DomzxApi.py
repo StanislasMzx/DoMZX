99af97dc37d9ebb17b9f904fda5acc408f0f1f39
MEZUREUX Stanislas <stanislas.mezureux@telecomnancy.eu>
2022-12-03 12:07:15 +0100

    CHORE: database functions in separate file

--- api/DomzxApi.py
+++ api/DomzxApi.py
@@ -3,40 +3,12 @@ from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import set_access_cookies
 from flask_jwt_extended import unset_jwt_cookies
-from flask import g, jsonify
+from flask import jsonify
 from flask_restful import Resource, reqparse
 from werkzeug.security import generate_password_hash, check_password_hash
 
-import sqlite3
+from api.database import *
 from api.gpioControl import *
-
-DATABASE = 'domzx.db'
-
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
-
-
-def query_db(query, args=(), one=False):
-    """
-    Retourne les tuples correspondants à la requête request avec les arguments éventuels args
-    """
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
-
-
-def update_db(request, args=[]):
-    """
-    Modifie la BD avec la requête request avec les arguments éventuels args
-    """
-    c = get_db().cursor()
-    c.execute(request, args)
-    get_db().commit()
 
 
 class Login(Resource):
